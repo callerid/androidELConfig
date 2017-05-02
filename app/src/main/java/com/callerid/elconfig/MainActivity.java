14c55ed74ff9d9031d0baa8b0620eb0ef17237ea
sam <sampenland@gmail.com>
2017-05-02 17:07:10 -0400

    v.1.1 - worked on layout - added features
    - worked on layout
    - added UDP receiver code

--- app/src/main/java/com/callerid/elconfig/MainActivity.java
+++ app/src/main/java/com/callerid/elconfig/MainActivity.java
@@ -1,22 +1,103 @@
 package com.callerid.elconfig;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends Activity implements ServiceCallbacks {
+
+    private String inString = "Waiting...";
+    private UDPListen mService;
+    private boolean mBound;
+
+    public void gotUDP(String inString){
+
+        // Reception of UDP string
+        // Handle all data
+
+        // Setup variables for use
+        String myData = inString;
+
+        String command;
+        Integer myLine=0;
+        String myType="";
+        String myIndicator="";
+
+        String myDuration="";
+        String myCheckSum="";
+        String myRings="";
+        String myDateTime="";
+        String myNumber="n/a";
+        String myName="";
+
+        // Check if matches a call record
+        Pattern myPattern = Pattern.compile(".*(\\d\\d) ([IO]) ([ES]) (\\d{4}) ([GB]) (.)(\\d) (\\d\\d/\\d\\d \\d\\d:\\d\\d [AP]M) (.{8,15})(.*)");
+        Matcher matcher = myPattern.matcher(myData);
+
+        if(matcher.find()){
+
+            myLine = Integer.parseInt(matcher.group(1));
+            myType = matcher.group(2);
+
+            if(myType.equals("I")||myType.equals("O")){
+
+                myIndicator = matcher.group(3);
+
+                myDuration = matcher.group(4);
+                myCheckSum = matcher.group(5);
+                myRings = matcher.group(6);
+                myDateTime = matcher.group(8);
+                myNumber = matcher.group(9);
+                myName = matcher.group(10);
+
+            }
+
+        }
+
+        // Check to see if call information is from a DETAILED record
+        Pattern myPatternDetailed = Pattern.compile(".*(\\d\\d) ([NFR]) {13}(\\d\\d/\\d\\d \\d\\d:\\d\\d:\\d\\d)");
+        Matcher matcherDetailed = myPatternDetailed.matcher(myData);
+
+        if(matcherDetailed.find()){
+
+            myLine = Integer.parseInt(matcherDetailed.group(1));
+            myType = matcherDetailed.group(2);
+
+            if(myType.equals("N")||myType.equals("F")||myType.equals("R")){
+                myDateTime = matcherDetailed.group(3);
+            }
+
+        }
+
+        // ------------- OUTPUT data to UI ------------------
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        
 
     }
 
@@ -25,6 +106,44 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.INTERNET},
+                1);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // bind to Service
+                    Intent intent = new Intent(this, UDPListen.class);
+                    bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+                } else {
+
+                    // permission denied. Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -41,4 +160,41 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Setup connection/binder to service
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+            UDPListen.LocalBinder binder = (UDPListen.LocalBinder) iBinder;
+            mService = binder.getService();
+            mBound = true;
+            mService.setCallbacks(MainActivity.this);
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+
+    };
+
+    // Link Display to Update so the UI gets updated through interface
+    @Override
+    public void display(String rString){
+
+        inString = rString;
+
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                gotUDP(inString);
+            }
+        });
+
+    }
+
 }
